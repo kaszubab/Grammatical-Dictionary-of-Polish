6604b360e91f89cc367c6f70ebc0af2db3390bf5
Konrad Dębiec <konradd@tutanota.com>
2020-06-09 09:52:06 +0200

    Moved test files into sub directory

--- src/test/test_dictionary_times.py
+++ src/test/test_dictionary_times.py
@@ -7,18 +7,19 @@ from src.dict import dictionary
 if __name__ == '__main__':
     print("-----------------------Test szybkości biblioteki - część jednosegmentowa-----------------------")
     for i in [100, 300, 600, 900]:
+        filename = "test_data/test_file_" + str(i) + ".txt"
         print("===============================================================================================")
         print("Rozmiar testowanego pliku: " + str(
-            math.ceil(os.stat("test_file_" + str(i) + ".txt").st_size / 1024)) + "KB")
+            math.ceil(os.stat(filename).st_size / 1024)) + "KB")
 
         number_of_lines = 0
-        with open("test_file_" + str(i) + ".txt", 'r', encoding="utf8") as f:
+        with open(filename, 'r', encoding="utf8") as f:
             for line in f:
                 number_of_lines += 1
         print("Ilość wpisów w testowanym pliku: ", number_of_lines)
 
         start_of_building = time()
-        test_dict = dictionary.Dictionary(["test_file_" + str(i) + ".txt"])
+        test_dict = dictionary.Dictionary([filename])
         end_of_building = time()
         print("Czas budowy struktury: " + str(end_of_building - start_of_building) + "s")
         print("Średni czas trwania przetworzenia jednego wpisu: "
